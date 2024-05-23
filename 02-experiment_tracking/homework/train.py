276debc2b857c43d3bee66d81ec44d83ef6df552
blackBagel <igal.chernov@gmail.com>
2024-05-23 16:07:39 +0000

    Changed m2/train.py to use autolog

--- 02-experiment_tracking/homework/train.py
+++ 02-experiment_tracking/homework/train.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
@@ -17,16 +18,31 @@ def load_pickle(filename: str):
     default="./output",
     help="Location where the processed NYC taxi trip data was saved"
 )
-def run_train(data_path: str):
+@click.option(
+    "--tracking_uri",
+    default="http://127.0.0.1:5000",
+    help="MLflow tracking_uri"
+)
+@click.option(
+    "--experiment_name",
+    default="02-experiment-tracking-homework",
+    help="MLflow experiment name"
+)
+def run_train(data_path: str, tracking_uri: str, experiment_name: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    mlflow.set_tracking_uri(tracking_uri)
+    mlflow.set_experiment(experiment_name)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    mlflow.sklearn.autolog()
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 
 if __name__ == '__main__':
