0560897cd4c17396e39070ab6e3479807e167e76
blackBagel <igal.chernov@gmail.com>
2024-07-06 16:07:29 +0000

    Changed code to support localstack

--- 06-best-practices/batch.py
+++ 06-best-practices/batch.py
@@ -21,13 +21,22 @@ def read_data(filename, categorical):
     
     return df
 
+def get_input_path(year, month):
+    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(year=year, month=month)
 
+
+def get_output_path(year, month):
+    default_output_pattern = 's3://nyc-duration/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet'
+    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    return output_pattern.format(year=year, month=month)
 
 
 def main(year, month):
 
-    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-    output_file = f'output/taxi_type=yellow_year={year:04d}_month={month:02d}.parquet'
+    input_file = get_input_path(year, month)
+    output_file = get_output_path(year, month)
 
 
     with open('model.bin', 'rb') as f_in:
